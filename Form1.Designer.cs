d96fa9e4276646f3ad5e5ead2d195e08b804c728
khaledOsama <ko968880@gmail.com>
2023-01-25 01:28:02 +0200

    we need to broandens it

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -113,9 +113,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(439, 457);
+            this.button1.Location = new System.Drawing.Point(415, 449);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(187, 74);
+            this.button1.Size = new System.Drawing.Size(251, 82);
             this.button1.TabIndex = 7;
             this.button1.Text = "LOGIN";
             this.button1.UseVisualStyleBackColor = true;
