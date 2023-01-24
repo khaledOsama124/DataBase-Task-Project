f4de90f7aba70b5e2c25c9bd43b702a8a93877b6
khaledOsama <ko968880@gmail.com>
2023-01-25 00:36:25 +0200

    we want to space the spaces

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(291, 615);
+            this.panel1.Size = new System.Drawing.Size(312, 615);
             this.panel1.TabIndex = 0;
             // 
             // Form1
@@ -45,7 +45,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(682, 615);
+            this.ClientSize = new System.Drawing.Size(855, 615);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
