743fb61ba4adc50d52ec8d885450c7810fb8d1d9
khaledOsama <ko968880@gmail.com>
2023-01-25 01:26:06 +0200

    we need to breadens it

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -112,9 +112,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(460, 482);
+            this.button1.Location = new System.Drawing.Point(439, 457);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 28);
+            this.button1.Size = new System.Drawing.Size(187, 74);
             this.button1.TabIndex = 7;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
