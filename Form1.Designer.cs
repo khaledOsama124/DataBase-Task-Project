c367f4c3da4de1fa762ca740a454935fe8986c45
khaledOsama <ko968880@gmail.com>
2023-01-25 00:47:49 +0200

    we need to put it up

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Teal;
-            this.label1.Location = new System.Drawing.Point(431, 102);
+            this.label1.Location = new System.Drawing.Point(435, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(141, 24);
             this.label1.TabIndex = 1;
