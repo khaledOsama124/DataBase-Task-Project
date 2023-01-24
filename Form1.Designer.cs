e85a81b26927dcf1b11fb1b63ed466283f530155
khaledOsama <ko968880@gmail.com>
2023-01-25 00:33:23 +0200

    we draw a squire

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(32, 183);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(291, 211);
+            this.panel1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -36,6 +44,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(682, 615);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -45,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
