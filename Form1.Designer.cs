db6d9546756c4d908f7b5e9daea15a91a4917316
khaledOsama <ko968880@gmail.com>
2023-01-25 01:30:55 +0200

    his color is Teal

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -113,6 +113,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Teal;
             this.button1.Location = new System.Drawing.Point(411, 481);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(251, 45);
