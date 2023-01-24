b5889e45fa4a2c212e4536e321e96f8d250fb894
khaledOsama <ko968880@gmail.com>
2023-01-25 01:05:33 +0200

    we need to boardens it

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -58,9 +58,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(413, 87);
+            this.pictureBox1.Location = new System.Drawing.Point(400, 87);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(209, 74);
+            this.pictureBox1.Size = new System.Drawing.Size(249, 146);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
