5a0827760ebdfec9f99bb4584304a3c2096d9b91
khaledOsama <ko968880@gmail.com>
2023-01-25 00:35:49 +0200

    his dock is left

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,9 +34,10 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Teal;
-            this.panel1.Location = new System.Drawing.Point(32, 183);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(291, 211);
+            this.panel1.Size = new System.Drawing.Size(291, 615);
             this.panel1.TabIndex = 0;
             // 
             // Form1
