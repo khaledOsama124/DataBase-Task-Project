9543263edf056e7f012f83a866d88736f16fd3ed
khaledOsama <ko968880@gmail.com>
2023-01-26 00:37:55 +0200

    his form border style is none

--- salaries.Designer.cs
+++ salaries.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // salaries
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "salaries";
             this.Text = "salaries";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
