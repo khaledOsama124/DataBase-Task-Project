d37cc2a38f5504d87a6760a8b3e862993777425e
khaledOsama <ko968880@gmail.com>
2023-01-25 16:22:46 +0200

    his fore color is white

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -46,7 +46,8 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(369, 57);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(530, 82);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 24);
             this.label1.TabIndex = 0;
