a55e7277a899b646c1e73c91388a67e3df4a4fd6
khaledOsama <ko968880@gmail.com>
2023-01-26 00:00:23 +0200

    his border style is none

--- Department.Designer.cs
+++ Department.Designer.cs
@@ -36,7 +36,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1257, 675);
             this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Department";
             this.Text = "Department";
             this.ResumeLayout(false);
