e9d1e81c08b8f65f02ae9258e0204cd517d897e9
khaledOsama <ko968880@gmail.com>
2023-01-26 19:53:44 +0200

    And we write a query

--- Department.cs
+++ Department.cs
@@ -37,6 +37,7 @@ namespace DataBase_Task_Project
                 {
                     string Dep = DepNameTb.Text;
                     string Query = "Insert into DepartmentTb1 values({'0'})";
+                    Query=String.Format()
                 }
             }
             catch (Exception ex)
