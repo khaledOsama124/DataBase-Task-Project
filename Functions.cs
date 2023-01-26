32cae27f9a0091179ed5e1e36f51c337c843b57c
khaledOsama <ko968880@gmail.com>
2023-01-26 18:02:07 +0200

    we write a private(sqlconnection , Sql command , Data Table

--- Functions.cs
+++ Functions.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataBase_Task_Project
 {
-    internal class Functions
+    class Functions
     {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        public Functions()
+        {
+
+        }
     }
 }
