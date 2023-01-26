af2073c1c9796e70c2ccc57a692aab530e74765f
khaledOsama <ko968880@gmail.com>
2023-01-26 18:07:10 +0200

    and we write a public functions()

--- Functions.cs
+++ Functions.cs
@@ -13,8 +13,11 @@ namespace DataBase_Task_Project
         private SqlConnection Con;
         private SqlCommand Cmd;
         private DataTable dt;
+        private SqlDataAdapter sda;
+        private string Constr;
         public Functions()
         {
+            Constr = @"";
 
         }
     }
