650982a9029e04dc987035cd05ff387c9af9a022
khaledOsama <ko968880@gmail.com>
2023-01-26 19:32:12 +0200

    and MessageBox.Show(ex.Message)

--- Department.cs
+++ Department.cs
@@ -34,6 +34,9 @@ namespace DataBase_Task_Project
             }
             catch (Exception ex)
             {
+                MessageBox.Show(ex.Message);
+            }
+            {
 
             }
 
