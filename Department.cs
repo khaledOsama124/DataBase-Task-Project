694acad8a964cef47cc9423833003b34ad0b39fd
khaledOsama <ko968880@gmail.com>
2023-01-26 19:46:53 +0200

    "insert into DepartmentTb1

--- Department.cs
+++ Department.cs
@@ -36,7 +36,7 @@ namespace DataBase_Task_Project
                 }else
                 {
                     string Dep = DepNameTb.Text;
-                    string Query=
+                    string Query = "Insert into DepartmentTb1";
                 }
             }
             catch (Exception ex)
